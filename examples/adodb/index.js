36be3e1ab7d94393a4b6884eb2aae590bfb1a498
Mike Kari Anderson <mikekari@Mikes-MacBook-Pro.local>
2017-07-29 17:06:34 -0500

    Created ADODB example

---
+++ examples/adodb/index.js
@@ -0,0 +1,16 @@
+var Database = require('database-js2').Connection;
+
+(async function main(){
+    let connection = new Database("database-js-adodb:///test.xls?Extended Properties='Excel 8.0;HDR=Yes;IMEX=1';");
+    let statement, results;
+
+    try {
+        statement = await connection.prepare("SELECT * FROM [Sheet1$A1:C52] WHERE State = ?");
+        results = await statement.query('South Dakota');
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        connection.close();
+    }
+})();
