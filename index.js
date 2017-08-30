e60ebe01d2169d7d50741b09693ea4168485d797
Michael Anderson <manderson@amesbury.com>
2017-08-30 10:01:21 -0500

    Exported the ParseConnection method.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Connection = require('./lib/connection');
 var StaticPool = require('./lib/staticPool');
 var DynamicPool = require('./lib/dynamicPool');
+var ParseConnection = require('./lib/parseConnection');
 
 module.exports = {
     Connection: Connection,
     StaticPool: StaticPool,
-    DynamicPool: DynamicPool
+    DynamicPool: DynamicPool,
+    URLParse: ParseConnection
 }
